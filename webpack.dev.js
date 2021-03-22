66eb8eb3cc7b96c6fa9b30a4b47bc710343ff9af
PA <52173001+xperience9@users.noreply.github.com>
2021-03-22 17:30:03 +0100

    changes

--- webpack.dev.js
+++ webpack.dev.js
@@ -11,7 +11,7 @@ module.exports = merge(common, {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: './index.html',
       favicon: './src/assets/favicon.png'
     })
   ],
